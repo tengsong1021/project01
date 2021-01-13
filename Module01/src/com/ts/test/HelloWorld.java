c516b5f4d5913a41cc7953d6c747b3798114271f
tengsong1021 <m17854119262@163.com>
2021-01-13 22:56:03 +0800

    Initial commit

---
+++ Module01/src/com/ts/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.ts.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("HelloWorld!" + i);
+        }
+
+        System.out.println("HelloWorld!" + "Tengsong");
+    }
+}
